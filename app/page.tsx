c2b9eece0bb8e325ada8aed88b06329d514eeb25
OmarMWarraich <earthling986@gmail.com>
2024-01-18 19:46:36 +0500

    feat: Declare Global Intersection Types for Transformed Supabase Data w/ Typescript

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,12 @@ export default async function Home() {
     redirect('/login')
   }
 
-  const { data } = await supabase.from('tweets').select('*, profiles(*), likes(*)')
+  const { data } = await supabase.from('tweets').select('*, author: profiles(*), likes(user_id)')
 
   const tweets =
     data?.map((tweet) => ({
       ...tweet,
+      author: Array.isArray(tweet.author) ? tweet.author[0] : tweet.author,
       user_has_liked_tweet: !!tweet.likes.find(
         (like) => like.user_id === session.user.id
       ),
@@ -32,7 +33,7 @@ export default async function Home() {
       {
         tweets?.map((tweet) => (
           <div key={tweet.id}>
-            <p>{tweet?.profiles?.name} {tweet?.profiles?.username}</p>
+            <p>{tweet.author.name} {tweet.author.username}</p>
             <h2>{tweet.title}</h2>
             <Likes tweet={tweet} />
           </div>
