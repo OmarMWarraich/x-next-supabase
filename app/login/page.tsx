6081dde411a627a7c19291b23afac8aa58936472
OmarMWarraich <earthling986@gmail.com>
2024-01-18 15:36:55 +0500

    feat: Impl protected routes for Authenticated Users w'/ Supabase Auth

---
+++ app/login/page.tsx
@@ -0,0 +1,22 @@
+import AuthButtonClient from '@/components/auth-button-client'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+type Props = {}
+
+const Login = async (props: Props) => {
+    const supabase = createServerComponentClient({ cookies })
+
+    const { data: { session } } = await supabase.auth.getSession()
+
+    if (session) {
+        redirect('/')
+    }
+  return (
+    <AuthButtonClient session={session} />
+  )
+}
+
+export default Login
